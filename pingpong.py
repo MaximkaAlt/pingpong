a9a3a2b3b1acfe54fefc81d97712a873d7bfa2be
MaximkaAlt <131990186+MaximkaAlt@users.noreply.github.com>
2023-05-11 19:26:10 +0300

    v1

--- pingpong.py
+++ pingpong.py
@@ -26,12 +26,8 @@ class Player(GameSprite):
             self.rect.y -= 5
         if keys_pressed[K_DOWN]and self.rect.y < 350:
             self.rect.y += 5
-class Enemy(GameSprite):
-    def zlo(self):
-        if self.rect.x < 500:
-            self.rect.x += self.speed
-        if self.rect.x == 500:
-            self.rect.x -= self.speed
+
+
 
 
 win = display.set_mode((700, 500))
@@ -41,8 +37,9 @@ bk = transform.scale(image.load("fon.png"), (700, 500))
 
 r1 = Player("r1.png", 5, 50, 180, 40, 150)
 r2 = Player("r2.png", 5, 600, 180, 40, 150)
-s1 = Enemy("ball.png", 200, 320, 180, 60, 60)
-
+s1 = Player("ball.png", 3, 320, 180, 60, 60)
+speed_x = 3
+speed_y = 3
 game = True
 
 clock = time.Clock()
@@ -58,8 +55,14 @@ while game:
     r2.reset()
     r2.dwij2()
     s1.reset()
-    s1.zlo()
-
-
+    if finish != True:
+        s1.rect.x+= speed_x
+        s1.rect.y+= speed_y
+    if s1.rect.y> 450 or s1.rect.y <0:
+        speed_y*= -1
+    if s1.rect.x> 650 or s1.rect.x <0:
+        speed_x*= -1
+    if sprite.collide_rect(r1, s1) or sprite.collide_rect(r2, s1):
+        speed_x*=-1
     display.update()          
     clock.tick(FPS)
